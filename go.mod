ee2710f68f8845fe8854a6c3d72ae577765dc1e6
Tomas Alejandro V. Falgui III <tomyfalguiiii@yahoo.com>
2021-07-04 14:09:05 +0800

    almost done

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tomyfalgui/nofap-bot
 
 go 1.16
+
+require github.com/mattn/go-sqlite3 v1.14.7
--- main.go
+++ main.go
@@ -2,13 +2,19 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strconv"
+	"strings"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 // Update is a Telegram object that the handler receives every time a user interacts with our bot
@@ -23,6 +29,15 @@ type Message struct {
 	Chat     Chat             `json:"chat"`
 	User     User             `json:"from"`
 	Entities *[]MessageEntity `json:"entities"`
+	Photo    *[]PhotoSize     `json:"photo"`
+}
+
+type PhotoSize struct {
+	FileId       string `json:"file_id"`
+	FileUniqueId string `json:"file_unique_id"`
+	Width        int    `json:"width"`
+	Height       int    `json:"height"`
+	FileSize     int    `json:"file_size"`
 }
 
 // IsCommand checks if a message is a command
@@ -56,6 +71,19 @@ func (e MessageEntity) IsCommand() bool {
 	return e.Type == "bot_command"
 }
 
+func (m *Message) CommandArguments() string {
+	if !m.IsCommand() {
+		return ""
+	}
+
+	entity := (*m.Entities)[0]
+	if len(m.Text) == entity.Length {
+		return "" // The command makes up the whole message
+	}
+
+	return m.Text[entity.Length+1:]
+}
+
 // User is a telegram object that represents a user
 type User struct {
 	Id        int    `json:"id"`
@@ -69,13 +97,60 @@ type Chat struct {
 	Id int `json:"id"`
 }
 
+var DB *sql.DB
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/bot-handler", handleTelegramWebhook)
+
+	db, err := sql.Open("sqlite3", "./tae.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	DB = db
+
+	result, err := DB.Exec(`
+		CREATE TABLE IF NOT EXISTS streaks (
+			id integer not null primary key,
+			streak integer,
+			streak_start text
+		);
+	 	`)
+	_ = result
+	if err != nil {
+		log.Printf("error creating table %q", err)
+		return
+	}
+
 	log.Printf("Listening on PORT 8000")
 	http.ListenAndServe(":8000", mux)
 
 }
+
+func getPictures() []string {
+	return []string{"AgACAgUAAxkBAAP_YOFLIVgSfI-IbSImZE1PiSAZTacAAp2sMRt_eAlXutnBAAGWoxhtAQADAgADcwADIAQ", "AgACAgUAAxkBAAIBAAFg4Us0ZZ5J3ixGenh39pNpUZlwZAACoKwxG394CVdAqmmcK6AvoQEAAwIAA3MAAyAE", "AgACAgUAAxkBAAIBAWDhS0evf1N9p0QdCvbv7R1Q6lj4AAKhrDEbf3gJVxljkJHrntoMAQADAgADcwADIAQ"}
+}
+
+func getUser(update *Update) (int, int) {
+	stmt, err := DB.Prepare("select id, streak from streaks where id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	var id int
+	var streak int
+	err = stmt.QueryRow(strconv.Itoa(update.Message.User.Id)).Scan(&id, &streak)
+	if err != nil {
+		log.Printf("%q", err)
+		return 0, 0
+	}
+	return id, streak
+}
+
+// func setStreak(update *Update) {
+// 	id, streak :=
+// }
 
 func handleTelegramWebhook(w http.ResponseWriter, r *http.Request) {
 	var update, err = parseTelegramRequest(r)
@@ -88,20 +163,120 @@ func handleTelegramWebhook(w http.ResponseWriter, r *http.Request) {
 		log.Print("No message")
 		return
 	}
-
 	transformed, err := json.MarshalIndent(update.Message, "", "    ")
 	if err != nil {
 		log.Printf("error printing user object")
 	}
-
 	log.Printf("[user obj]: %s", transformed)
 	if update.Message.IsCommand() {
 		var text string = ""
 		switch update.Message.Command() {
-		case "tite":
+		case "start":
 			{
-				text = "maliit tite mo whahaha"
+				id, streak := getUser(update)
+
+				if id == 0 {
+					tx, err := DB.Begin()
+					if err != nil {
+						log.Fatal(err)
+					}
+					stmt, err := tx.Prepare("insert into streaks(id, streak, streak_start) values(?, ?, ?)")
+					if err != nil {
+						log.Fatal(err)
+					}
+					defer stmt.Close()
+					t := time.Now().Local()
+					s := t.Format("2006-01-02")
+					result, err := stmt.Exec(update.Message.User.Id, 0, s)
+					if err != nil {
+						log.Fatal(err)
+					}
+					_ = result
+					tx.Commit()
+					text = "Created babyyy"
+				} else {
+
+					text = fmt.Sprintf("Your streak is this loooong: %d", streak)
+				}
 			}
+		case "streak":
+			{
+
+				id, streak := getUser(update)
+				if id == 0 {
+					tx, err := DB.Begin()
+					if err != nil {
+						log.Fatal(err)
+					}
+					stmt, err := tx.Prepare("insert into streaks(id, streak, streak_start) values(?, ?, ?)")
+					if err != nil {
+						log.Fatal(err)
+					}
+					defer stmt.Close()
+					t := time.Now().Local()
+					s := t.Format("2006-01-02")
+					result, err := stmt.Exec(update.Message.User.Id, 0, s)
+					if err != nil {
+						log.Fatal(err)
+					}
+					_ = result
+					tx.Commit()
+					text = fmt.Sprintf("Your streak is this loooong: %d", 0)
+				} else {
+					text = fmt.Sprintf("Your streak is this loooong: %d", streak)
+				}
+			}
+
+		case "horny":
+			{
+				// pictures := getPictures()
+				// var telegramResponseBody, errTelegram = sendPhotoToTelegramChat(update.Message.Chat.Id, pictures[rand.Intn(len(pictures))])
+
+				// if errTelegram != nil {
+				// 	log.Printf("got error %s from telegram, reponse body is %s", errTelegram.Error(), telegramResponseBody)
+				// } else {
+				// 	log.Printf("Success with sending image to %d", update.Message.Chat.Id)
+				// }
+				// return
+				text = "Stop. Dont do it."
+			}
+		case "setstreak":
+			{
+				processedArgCommand := update.Message.CommandArguments()
+				processedArgCommand = strings.Trim(processedArgCommand, " ")
+				if argLength := len(processedArgCommand); argLength == 0 {
+					text = "Please provide an argument"
+				} else {
+					converted, err := strconv.Atoi(processedArgCommand)
+					if err != nil {
+						text = "Please provide a number"
+					}
+					if converted <= 0 {
+
+						text = "Please provide a positive number"
+					} else {
+
+						text = "Alls well"
+					}
+				}
+				log.Printf("%s", update.Message.CommandArguments())
+			}
+		case "help":
+			{
+				text = `
+			These are my commands.
+			
+			/help - Get help
+			/streak - Get your current streak
+			/setstreak - Set streak
+			/horny - Get horny pic
+			`
+			}
+		default:
+			{
+				text = "Broooo. command??????"
+			}
+
 		}
 		var telegramResponseBody, errTelegram = sendTextToTelegramChat(update.Message.Chat.Id, text)
 
@@ -140,6 +315,61 @@ func sendTextToTelegramChat(chatId int, text string) (string, error) {
 		return "", err
 	}
 
+	defer response.Body.Close()
+
+	var bodyBytes, errRead = io.ReadAll(response.Body)
+	if errRead != nil {
+		log.Printf("error in parsing telegram answer %s", errRead.Error())
+		return "", err
+	}
+
+	bodyString := string(bodyBytes)
+	log.Printf("Body of Telegram Response: %s", bodyString)
+
+	return bodyString, nil
+}
+
+type PhotoArguments struct {
+	Photo  Photo  `json:"photo"`
+	ChatId string `json:"chat_id"`
+}
+
+type Photo struct {
+	FileId string `json:"file_id"`
+}
+
+func sendPhotoToTelegramChat(chatId int, fileId string) (string, error) {
+	log.Printf("Sending image to chat id: %d", chatId)
+
+	var telegramApi string = "https://api.telegram.org/bot" + os.Getenv("BOT_TOKEN") + "/sendPhoto"
+
+	photo := Photo{FileId: fileId}
+	photoArguments := PhotoArguments{Photo: photo, ChatId: strconv.Itoa(chatId)}
+
+	data, err := json.Marshal(photoArguments)
+
+	if err != nil {
+		return "", err
+	}
+
+	transformed, err := json.MarshalIndent(photoArguments, "", "    ")
+	if err != nil {
+		log.Printf("error printing url object")
+	}
+	log.Printf("[url obj]: %s", transformed)
+
+	client := &http.Client{}
+	r, err := http.NewRequest("POST", telegramApi, strings.NewReader(string(data)))
+
+	if err != nil {
+		log.Printf("error when creating post image to the chat: %s", err.Error())
+		return "", err
+	}
+	response, err := client.Do(r)
+	if err != nil {
+		log.Printf("error when posting image to the chat: %s", err.Error())
+		return "", err
+	}
 	defer response.Body.Close()
 
 	var bodyBytes, errRead = io.ReadAll(response.Body)
